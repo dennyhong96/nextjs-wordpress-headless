d6c18d327ede8e6f0f399385bef5550e352cfe1f
Denny Hong <denny@beacondigitalmarketing.com>
2020-12-06 19:47:22 -0800

    Featch menus in nextjs

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,6 @@ import client from "../src/apollo/client";
 import "../src/styles/index.scss";
 
 function MyApp({ Component, pageProps }) {
-  console.log(`${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql`);
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
