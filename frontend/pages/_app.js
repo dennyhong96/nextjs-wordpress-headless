45808e6cd9189471d676b927e39b3fc40231e3d7
Denny Hong <denny@beacondigitalmarketing.com>
2020-12-06 19:17:44 -0800

    Setup apolloclient

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
+import { ApolloProvider } from "@apollo/client";
+
+import client from "../src/apollo/client";
 import "../src/styles/index.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  console.log(`${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql`);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
